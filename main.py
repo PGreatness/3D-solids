ba22bd4487293e05642e033ae8257b6932e48eef
Ahnaf Hasan <ahasan1@stuy.edu>
2019-04-16 20:25:17 -0400

    add base files

---
+++ main.py
@@ -0,0 +1,19 @@
+from display import *
+from draw import *
+from parser import *
+from matrix import *
+import math
+
+screen = new_screen()
+zbuffer = new_zbuffer()
+color = [ 0, 255, 0 ]
+edges = []
+polygons = []
+t = new_matrix()
+ident(t)
+csystems = [ t ]
+
+
+parse_file( 'script', edges, polygons, csystems, screen, zbuffer, color )
+
+
